1aaef0f90769fff15f3a3cca1db6c8403b131aa1
mrpoor <greg15001@gmail.com>
2019-10-25 18:40:47 -0400

    Added csrf token

--- forms.py
+++ forms.py
@@ -1,17 +1,18 @@
-from wtforms import Form, BooleanField, StringField, PasswordField, validators, TextAreaField
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, StringField, PasswordField, validators, TextAreaField
 
 
-class SpellCheckForm(Form):
+class SpellCheckForm(FlaskForm):
     text = TextAreaField("Input text here", id="inputtext", validators=[validators.DataRequired()])
 
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     username = StringField('Username', id="uname", validators=[validators.Length(min=4, max=25), validators.DataRequired()])
     phonenumber = StringField('Phone Number', id="2fa", validators=[validators.DataRequired()])
     password = PasswordField('New Password', id="pword", validators= [validators.DataRequired()])
 
 
-class RegistrationForm(Form):
+class RegistrationForm(FlaskForm):
     username = StringField('Username', id="uname",validators=[validators.Length(min=4, max=25), validators.DataRequired()])
     phonenumber = StringField('Phone Number', id="2fa", validators=[validators.DataRequired()])
     # password = PasswordField('New Password', id="pword",validators= [
