54b312f0856030ec6dc2dbec45603aed86104281
mrpoor <greg15001@gmail.com>
2019-10-30 16:59:13 -0400

    Added some more unit tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -10,10 +10,9 @@ from models import User
 
 @pytest.fixture
 def client():
-    db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
+    app.app.config['DATABASE'] = tempfile.mkstemp()
     app.app.config['TESTING'] = True
     app.app.config['WTF_CSRF_ENABLED'] = False
-
     app_client = app.app.test_client()
     app_context = app.app.app_context()
     app_context.push()
@@ -45,7 +44,6 @@ def test_register_form(client):
 
 def test_login_form(client):
     rv = client.get('/login')
-    print(rv)
     assert b'id="pword"' in rv.data
 
 
@@ -72,10 +70,47 @@ def test_login_success(client, init_db,init_bcrypt):
 
 
 def test_registration_success(client, init_db):
-    rv = client.post('/register', data=dict(
+    client.post('/register', data=dict(
         username="greg1",
         password="greg_p1",
         phonenumber="333"
     ))
     user = User.query.filter_by(name="greg1").first()
     assert user.name == "greg1"
+
+
+def test_spell_check_auth(client):
+    rv = client.get('/spell_check')
+    assert rv.status_code == 401
+
+
+def test_spell_check_get(client, init_db, init_bcrypt):
+    bcrypt_hash = init_bcrypt.generate_password_hash("greg_p")
+    user = User(name="greg", phone_number="333", password_hash=bcrypt_hash)
+    init_db.session.add(user)
+    init_db.session.commit()
+    rv = client.post('/login', data=dict(
+        username="greg",
+        password="greg_p",
+        phonenumber="333"
+    ))
+    assert b"Success" in rv.data
+    rv2 = client.get('/spell_check')
+    assert b'id="inputtext"' in rv2.data
+
+
+def test_spell_check_success(client, init_db, init_bcrypt):
+    bcrypt_hash = init_bcrypt.generate_password_hash("greg_p")
+    user = User(name="greg", phone_number="333", password_hash=bcrypt_hash)
+    init_db.session.add(user)
+    init_db.session.commit()
+    rv = client.post('/login', data=dict(
+        username="greg",
+        password="greg_p",
+        phonenumber="333"
+    ))
+    assert b"Success" in rv.data
+    rv2 = client.post('/spell_check', data=dict(
+        text="hello bye bds"
+    ))
+    assert b'hello bye bds' in rv2.data
