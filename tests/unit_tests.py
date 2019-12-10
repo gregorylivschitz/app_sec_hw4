0a1830250bfb816bca6a2d7ee52ae5e5de3e0f09
mrpoor <greg15001@gmail.com>
2019-12-10 02:02:35 -0500

    Added some more unit tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -114,3 +114,66 @@ def test_spell_check_success(client, init_db, init_bcrypt):
         text="hello bye bds"
     ))
     assert b'hello bye bds' in rv2.data
+
+
+def test_spell_check_sql_inject(client, init_db,init_bcrypt):
+    bcrypt_hash = init_bcrypt.generate_password_hash("greg_p")
+    user = User(name="greg", phone_number="333", password_hash=bcrypt_hash)
+    init_db.session.add(user)
+    init_db.session.commit()
+    rv = client.post('/login', data=dict(
+        username="greg",
+        password="greg_p",
+        phonenumber="333"
+    ))
+    assert b"Success" in rv.data
+    rv2 = client.post('/spell_check', data=dict(
+        text="hello; drop table user; --"
+    ))
+    user = User.query.get(1)
+    assert user is not None
+
+
+def test_logs(client, init_db, init_bcrypt):
+    admin_user = User(name="admin", phone_number="12345678901",
+                      password_hash=b'$2b$12$0HVKc/rDTJOYXRaGdFBPeu.ZdEH0F3uMvUV/AEmoKLXDHUp7pQb8O')
+    init_db.session.add(admin_user)
+    init_db.session.commit()
+    bcrypt_hash1 = init_bcrypt.generate_password_hash("greg_p")
+    user2 = User(name="greg", phone_number="333", password_hash=bcrypt_hash1)
+    init_db.session.add(user2)
+    init_db.session.commit()
+    rv2 = client.post('/login', data=dict(
+        username="greg",
+        password="greg_p",
+        phonenumber="333"
+    ))
+    assert b"Success" in rv2.data
+    rv = client.post('/login', data=dict(
+        username="admin",
+        password="Administrator@1",
+        phonenumber="12345678901"
+    ))
+    assert b"Success" in rv.data
+    rv4 = client.get('/admin/history/log/2')
+    print(rv4.data)
+    assert b"time" in rv4.data
+
+
+def test_query_history(client, init_db, init_bcrypt):
+    bcrypt_hash = init_bcrypt.generate_password_hash("greg_p")
+    user = User(name="greg", phone_number="333", password_hash=bcrypt_hash)
+    init_db.session.add(user)
+    init_db.session.commit()
+    rv = client.post('/login', data=dict(
+        username="greg",
+        password="greg_p",
+        phonenumber="333"
+    ))
+    assert b"Success" in rv.data
+    rv2 = client.post('/spell_check', data=dict(
+        text="hello bye bds"
+    ))
+    assert b'hello bye bds' in rv2.data
+    rv4 = client.get('/history/query1')
+    assert b"querytext" in rv4.data
